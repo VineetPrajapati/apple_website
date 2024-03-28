f42602bdd7f32d238d5f2e3c5008f2022829dac5
Vineet <infinite2202@gmail.com>
2024-03-28 14:33:24 +0530

    apple-website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
-
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
@@ -18,8 +18,7 @@ const App = () => {
       <HowItWorks />
       <Footer />
     </main>
-
-  )
+  );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
